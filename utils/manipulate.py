5a69b63e404fad4fd6ad03a477e90661177fe86d
Michaelszj <2100012962@stu.pku.edu.cn>
2023-05-21 15:37:10 +0800

    modify 1

--- utils/manipulate.py
+++ utils/manipulate.py
@@ -26,3 +26,20 @@ def c_shadow(x):
 
 def reverse(x):
     return x[::-1]
+
+
+def nearest(x):
+    t = random.randint(0, 1)
+    y = x % 12
+    if y == 0 or y == 2 or y == 4 or y == 7 or y == 9:
+        return x
+    if y == 1 or y == 3 or y == 8:
+        return x-1+t*2
+    if y == 5 or y == 10:
+        return x-1
+    if y == 6 or y == 11:
+        return x+1
+
+
+def clamp(x):
+    return np.where(x >= 21, nearest(x), x)

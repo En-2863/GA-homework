448cb81a17482f8efc15d719a6490212cfcda119
Michaelszj <2100012962@stu.pku.edu.cn>
2023-05-21 15:41:08 +0800

    modify 2

--- utils/manipulate.py
+++ utils/manipulate.py
@@ -1,29 +1,29 @@
 import numpy as np
 import random
 
-
+#杂交水稻
 def crossOver(x, y):
     m = np.random.random(32)
     result = np.where(m < 0.5, x, y)
     return result
 
-
+#平移变换
 def shift(x, n):
     return np.where(x >= 21, x+n, x)
 
-
+#随机平移
 def randomShift(x):
     return shift(x, random.randint(-12, 12))
 
-
+#倒影变换
 def shadow(x, n):
     return np.where(x >= 21, n*2-x, x)
 
-
+#围绕中央c倒影
 def c_shadow(x):
     return shadow(x, 60)
 
-
+#逆行变换
 def reverse(x):
     return x[::-1]
 
@@ -40,6 +40,6 @@ def nearest(x):
     if y == 6 or y == 11:
         return x+1
 
-
+#靠拢五声音阶
 def clamp(x):
     return np.where(x >= 21, nearest(x), x)
